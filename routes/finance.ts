c3292b58d6a254301da75d6c6b6f1549b5b8ae85
Mojeeto <mojtaba01nouri@gmail.com>
2024-08-19 10:49:32 +0330

    [feat] finance routes

---
+++ routes/finance.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+const financeRouter = Router();
+
+// GET::Get all finance data about related user
+financeRouter.get("/finance");
+
+// POST::Create new finance record
+financeRouter.post("/finance");
+
+// Patch::Update finance record
+financeRouter.patch("/finance");
+
+// DELETE::Delete finance record
+financeRouter.delete("/finance");
+
+export default financeRouter;
